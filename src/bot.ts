8b40eb153b4e1f0a4e8585ce8d48be32f9b2a079
John Q. Herman <johnqherman@gmail.com>
2023-08-15 21:46:39 -0400

    Heroku hotfixes

--- src/bot.ts
+++ src/bot.ts
@@ -9,6 +9,7 @@ import Discord, {
 import fs from 'node:fs';
 import path from 'node:path';
 
+require('./server'); // keep server alive on heroku
 require('dotenv').config();
 
 // extend Client class to add commands property
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => {
+    res.send('bot is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
